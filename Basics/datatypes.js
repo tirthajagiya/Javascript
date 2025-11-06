10bc6dade8632c33bfba663965061b113bf92893
Tirth Ajagiya <tirthajagiya5618@gmail.com>
2025-11-06 12:32:22 +0000

    learn datatype

---
+++ Basics/datatypes.js
@@ -0,0 +1,25 @@
+let name = "XYZ";
+let age = 18;
+let isMarried = false;
+let address;
+let salary = null;
+
+console.log(address); // undefined
+
+console.log(typeof name);
+console.log(typeof address);
+console.log(typeof salary);
+
+// typeof undefined -> undefined
+// typeof null -> object
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// non-primitive data type
+// object
