68aed532688f477f5f1b35ea9ea0e26050f958d1
Tirth Ajagiya <tirthajagiya5618@gmail.com>
2025-11-15 17:28:43 +0000

    learn Math in js

---
+++ Basics/numberAndMaths.js
@@ -0,0 +1,35 @@
+const salary = 1000;
+// console.log(salary);
+// console.log(typeof salary);
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(typeof balance);
+
+// console.log(balance.toString().length);
+
+const num = 123.8933;
+// console.log(num.toPrecision(4));
+
+const hudrends = 100000
+// console.log(hudrends);
+// console.log(hudrends.toLocaleString('en-IN'));
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(1,2,3));
+// console.log(Math.max(2,3,4));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10+1));
+
+const min = 10; max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+const minNumber = 1; maxNumber = 6;
+
+console.log(Math.floor(Math.random()*(maxNumber-minNumber+1)+minNumber));
