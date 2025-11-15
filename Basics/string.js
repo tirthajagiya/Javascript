98c62de0ed2ddd05d32b6b2b62011ad4f78e489c
Tirth Ajagiya <tirthajagiya5618@gmail.com>
2025-11-15 17:03:44 +0000

    learn String

---
+++ Basics/string.js
@@ -0,0 +1,22 @@
+const name = "tirth"
+const age = "19"
+// console.log(typeof name);
+
+// console.log("my name is:"+name+" and my age is:"+age);
+// console.log(`My name is:${name} and my age is:${age}`);
+
+const panNumber = new String('ABC12XYZ');
+// console.log(typeof panNumber);
+// console.log(panNumber);
+// console.log(panNumber[0]);
+// console.log(panNumber.__proto__);
+
+console.log(panNumber.length);
+console.log(panNumber.toUpperCase());
+console.log(panNumber.toLowerCase());
+console.log(panNumber.charAt(2));
+console.log(panNumber.indexOf('A'));
+console.log(panNumber.substring(2));
+// console.log(panNumber.substring(1,5));
+
+//replace,trim,substr,slice,includes
